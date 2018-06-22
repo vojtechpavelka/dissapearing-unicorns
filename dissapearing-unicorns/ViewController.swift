447febb82c188af5f4cb1529c29d7d18f20a33d8
Vojtěch Pavelka <vojtechpavelka@gmail.com>
2018-06-22 19:26:45 +0200

    Initial Commit

---
+++ dissapearing-unicorns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dissapearing-unicorns
+//
+//  Created by Vojtěch Pavelka on 22.06.18.
+//  Copyright © 2018 Vojtěch Pavelka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
