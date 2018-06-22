627255f81559d4516a938af8216e27c83bdc80b3
Vojtěch Pavelka <vojtechpavelka@gmail.com>
2018-06-22 20:56:40 +0200

    Created PlayerData file

---
+++ dissapearing-unicorns/PlayerData.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerData.swift
+//  dissapearing-unicorns
+//
+//  Created by Vojtěch Pavelka on 22.06.18.
+//  Copyright © 2018 Vojtěch Pavelka. All rights reserved.
+//
+
+import UIKit
+
+class PlayerData: NSObject {
+
+}
